8673f2308a2065584782f6908b5c9e3b5e3c6ab1
WanderlustApp <noreply@github.com>
2026-06-19 16:08:17 +0530

    Added root route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
 
 app.use("/api/auth",authRouter);
 
